0a0ea2c5d70197f70796a047b123e6861b74f76a
GerardRibas1 <57219089+GerardRibas1@users.noreply.github.com>
2021-10-20 19:56:00 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function confirmaRegistre(){
+    alert("confirmant registre...");
+    document.getElementById("formDiv").innerHTML = "<p class='important'>T'has registrat amb èxit!</p>";
+    console.log("registre confirmat!");
+    return false;
+}   
